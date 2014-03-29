aea290e44a11ac0f522c1bc8b10f39d410913df6
Varun Bhartia <vbhartia@mba2014.hbs.edu>
2014-03-29 14:51:17 -0400

    fix staging

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -10,7 +10,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 	  provider :facebook, '1471364669746168', 'daf410d3bcf59696b32aa36bbc3ca504', :display => "popup"
 	  provider :linkedin, '77n7xe5k86jqwd', '1kdbgL99EJ0pvmtt'
 		puts 'local host'
-	elsif Socket.gethostname.match('herokuapp.com')
+	else
       puts 'staging'
 	  provider :facebook, '552978684817030', '9d80da4846a2791a79df262cb25a9fa9', :display => "popup"
 
