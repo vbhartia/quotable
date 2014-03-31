db6cc59d75441b7cf2b136bfa359ed22ea1b65e0
Varun Bhartia <vbhartia@mba2014.hbs.edu>
2014-03-30 23:31:55 -0400

    add authors

---
+++ scrapping_and_deployment_scripts/associate_author_quote.rb
@@ -0,0 +1,22 @@
+count = 0
+
+Quote.all.each do |quote|
+
+	count = count + 1
+
+	puts count
+
+	author_from_quote_slug = quote.author.gsub(' ', '_')
+
+	author_to_associate = Author.where(:friendly_name => quote.author).first
+
+	if author_to_associate != nil
+
+		#author_to_associate.friendly_name = quote.author
+
+		#author_to_associate.save!
+
+		quote.authors << author_to_associate
+	end
+
+end
